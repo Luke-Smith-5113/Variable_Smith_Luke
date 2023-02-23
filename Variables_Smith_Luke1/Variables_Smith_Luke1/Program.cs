b812c76e3eec72345df704540b1e0dfb2a6dbd23
Luke-Smith-5113 <114005746+Luke-Smith-5113@users.noreply.github.com>
2023-02-23 09:58:53 -0600

    Add files via upload

---
+++ Variables_Smith_Luke1/Variables_Smith_Luke1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Variables_Smith_Luke
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declaring and initializing variables
+            int faveNumber;
+            bool isJumping = false, isRunning = false;
+            float myFloat;
+            faveNumber = 6;
+            myFloat = 729.21f;
+            double finalGrade = 100;
+
+            //printing all variables in seperate lines
+            Console.WriteLine(faveNumber);
+            Console.WriteLine(isJumping);
+            Console.WriteLine(isRunning);
+            Console.WriteLine(myFloat);
+            Console.WriteLine(finalGrade);
+        }
+    }
+}
