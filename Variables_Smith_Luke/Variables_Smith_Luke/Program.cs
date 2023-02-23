750f102006368d1e5a4290de9419a7091e394fb2
Luke-Smith-5113 <114005746+Luke-Smith-5113@users.noreply.github.com>
2023-02-23 09:35:38 -0600

    Add files via upload

---
+++ Variables_Smith_Luke/Variables_Smith_Luke/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Variables_Smith_Luke
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
